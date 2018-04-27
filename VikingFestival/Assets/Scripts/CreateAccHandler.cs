f13b9580f39995bb1b2a6c83d3919c22f7d5fba2
fihpinato <fpinatoaguiar@gmail.com>
2018-04-27 11:11:53 -0300

    Update in Create Acc

---
+++ VikingFestival/Assets/Scripts/CreateAccHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateAccHandler : MonoBehaviour {
+
+    public Button SignupButton;
+    public InputField email;
+    public InputField senhaConfirma;
+
+    public InputField senha;
+    public InputField dataNascimento;
+
+    public InputField nomeInputField;
+
+    void Start () {
+        if (SignupButton != null)
+            SignupButton.onClick.AddListener(() => SimpleAddedFirebase.Instance.Signup(senha.text, senhaConfirma.text, email.text, nomeInputField.text, dataNascimento.text));
+    }
+}
--- VikingFestival/Assets/Scripts/LoginHandler.cs
+++ VikingFestival/Assets/Scripts/LoginHandler.cs
@@ -5,27 +5,18 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LoginHandler : MonoBehaviour {
-	public Button CadatroB;
 
-    public Button SignupButton;
     public Button LoginButton;
+
     public InputField email;
-	public InputField senhaConfirma;
-
     public InputField senha;
-	public InputField dataNascimento;
-
-	public InputField nomeInputField;
-
 
     void Start () {
-        if (SignupButton != null)
-			SignupButton.onClick.AddListener(() => SimpleAddedFirebase.Instance.Signup(senha.text,senhaConfirma.text, email.text, nomeInputField.text, dataNascimento.text));
-
         if (LoginButton != null)
             LoginButton.onClick.AddListener(() => SimpleAddedFirebase.Instance.Login(senha.text, email.text));
 		
     }
+
 	public void BotaoCadastro(){
 		SceneManager.LoadScene("TelaCadastro", LoadSceneMode.Single);
 	}
